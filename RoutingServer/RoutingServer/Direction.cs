171cd00d7eee7b759eefbff27ab07a478247e816
Weel06 <60885609+weelBenAissa@users.noreply.github.com>
2022-11-29 16:44:00 +0100

    blabla push 1

---
+++ RoutingServer/RoutingServer/Direction.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Text;
+using System.Threading.Tasks;
+using Swashbuckle.AspNetCore.SwaggerGen;
+
+namespace RoutingServer
+{
+    [DataContract] 
+   internal class Direction
+    {
+        [DataContract]
+        public class features
+        {
+            [DataMember]
+            public string type { get; set; }
+            [DataMember]
+            public Properties properties { get; set; }
+            [DataMember]
+            public string geometry { get; set; }
+        }
+        [DataContract]
+        public class Properties
+        {
+            [DataMember]
+            Segment segments { get; set; }
+            [DataMember]
+            Geometry geometry { get; set; }
+            [DataMember]
+            Summary summary { get; set; }
+
+        }
+        [DataContract]
+        public class Summary {
+            [DataMember]
+            public double distance { get; set; }
+            [DataMember]
+            public double duration { get; set; }
+        }
+        [DataContract]
+        public class Geometry
+        {
+            [DataMember]
+            List<double> coordinates { get; set; }
+        }
+        [DataContract]
+        public class Segment
+        {
+            [DataMember]
+            double distance { get; set; }
+            [DataMember]
+            double duration { get; set; }
+            [DataMember]
+            List<Step> steps { get; set; }
+        }
+        public class Step
+        {
+            [DataMember]
+            double distance { get; set; }
+            [DataMember]
+            double duration { get; set; }
+            [DataMember]
+            string type { get; set; }
+            [DataMember]
+            string instruction { get; set; }
+            [DataMember]
+            string name { get; set; }
+            [DataMember]
+            List<int> way_points { get; set; }
+            
+        }
+
+
+    }
+    }
+    
+
---
+++ RoutingServer/RoutingServer/Service.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using static System.Collections.Specialized.BitVector32;
+
+
+namespace RoutingServer
+{
+  
+    internal class Service : IService
+    {
+        private static HttpClient client = new HttpClient();
+        private List<station> stations = new List<station>();
+   
+        string KEY = "5b3ce3597851110001cf624810d1e3dd14444e7890e65060cb520bac";
+        string PATH = "https://api.openrouteservice.org";
+       public Direction GetItinerary(Double depart, Double arrive,string profile)  
+        {
+            if (profile == null)
+            {
+                profile = "driving-car";
+            }
+            string url = PATH + "/v2/directions/" + profile + "?api_key=" + KEY;
+            string json = "{\"coordinates\":[[" + depart + "],[" + arrive + "]],\"profile\":\"" + profile + "\",\"preference\":\"fastest\",\"format\":\"geojson\"}";
+            HttpResponseMessage response = client.GetAsync(url).GetAwaiter().GetResult();
+            return response.Content.ReadAsAsync<Direction>().GetAwaiter().GetResult();
+
+        }
+
+    }
+    public class contract
+    {
+        string name { get; set; }
+    }
+    public class station
+    {
+        public int number { get; set; }
+        public string name { get; set; }
+        public Position position { get; set; }
+    }
+    public class Position
+    {
+        public Double latitude { get; set; }
+        public Double longitude { get; set; }
+    }
+
+}
